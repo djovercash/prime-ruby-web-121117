fb0b0b3522cabb6527e5ee9caee3d48fc7bf6da9
github username <github email address>
2017-12-03 14:29:58 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 def prime? (integer)
-    return false if integer <= 1
+  if integer <= 1
+    return false
+  else
     Math.sqrt(integer).to_i.downto(2).each {|i| return false if integer % i == 0}
     true
+  end
 end
-
